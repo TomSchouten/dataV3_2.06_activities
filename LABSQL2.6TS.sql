2daff11d55a0b870fc5dd4a383b726a3a1ca32a3
TomSchouten <93996357+TomSchouten@users.noreply.github.com>
2022-01-19 20:04:09 +0100

    Add files via upload

--- LABSQL2.6TS.sql
+++ LABSQL2.6TS.sql
@@ -1,21 +1,20 @@
 use sakila;
 
-
 --     In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list.
 
 SELECT DISTINCT last_name
-FROM actor;
+FROM 	  		actor;
 
 --     Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors where the last name was present more than once
 
-SELECT last_name, COUNT(last_name)
-FROM actor
+SELECT   last_name, COUNT(last_name)
+FROM 	 actor
 GROUP BY last_name;
 
 --     Using the rental table, find out how many rentals were processed by each employee.
 
-SELECT Staff_id, COUNT(rental_id) AS 'amount_of_rentals_processed'
-FROM rental
+SELECT   Staff_id, COUNT(rental_id) AS 'amount_of_rentals_processed'
+FROM	 rental
 GROUP BY staff_ID;
 
 --     Using the film table, find out how many films were released each year.
@@ -46,8 +45,8 @@ HAVING    AVG(length) >120;
 
 --     Rank films by length (filter out the rows that have nulls or 0s in length column). In your output, only select the columns title, length, and the rank.
 
-SELECT title, length, ROW_NUMBER() 
-OVER (ORDER BY length DESC) as 'rank'
-FROM sakila.film
+SELECT   title, length, ROW_NUMBER() 
+OVER     (ORDER BY length DESC) as 'rank'
+FROM     sakila.film
 ORDER BY length DESC;
 
